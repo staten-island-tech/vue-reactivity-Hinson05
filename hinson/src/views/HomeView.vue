a8e7307e37bb6457f9285df3d51589beef521c19
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2023-03-09 13:59:33 -0500

    a

--- hinson/src/views/HomeView.vue
+++ hinson/src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <script>
-import TheWelcome from "../components/TheWelcome.vue";
+export default {
+  data() {
+    return {
+      text: "",
+    };
+  },
+  methods: {
+    onInput(e) {
+      this.text = e.target.value;
+    },
+  },
+};
 </script>
 
 <template>
+  <input :value="text" @input="onInput" placeholder="Type here" />
+  <p>{{ text }}</p>
   <main>
     <TheWelcome />
+    <h1>Hello</h1>
   </main>
 </template>
