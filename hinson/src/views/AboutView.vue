4e7897f0acf5cb7c00ac89e9e0af21f3c822cfbd
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2023-03-10 13:59:51 -0500

    a

--- hinson/src/views/AboutView.vue
+++ hinson/src/views/AboutView.vue
@@ -1,15 +1,26 @@
+<script setup>
+import { ref } from "vue";
+
+const awesome = ref(true);
+const count = ref(0);
+
+function toggle() {
+  awesome.value = !awesome.value;
+  count.value++;
+}
+</script>
 <template>
   <div class="about">
-    <h1>This is not an about page</h1>
-    <button>blue</button>
+    <h2>count is: {{ count }}</h2>
   </div>
+  <button @click="toggle">purchase</button>
+  <h1 v-if="awesome">Add item to cart</h1>
+  <h1 v-else>Item Added</h1>
 </template>
-
 <style>
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
-    display: flex;
     align-items: center;
   }
 }
