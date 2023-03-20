918d3b492e530ad79a7c9852b7830217001f9740
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2023-03-20 14:00:23 -0400

    a

--- hinson/src/views/AboutView.vue
+++ hinson/src/views/AboutView.vue
@@ -37,8 +37,16 @@ export default {
 };
 </script>
 <template>
-  <div class="about">
-    <h2>count is: {{ count }}</h2>
+  <div class="shop">
+    <h2>
+      <img
+        id="icon"
+        src="https://cdn.onlinewebfonts.com/svg/img_208967.png"
+        width="35"
+        height="35"
+      />
+      {{ count }}
+    </h2>
   </div>
   <button v-if="awesome" @click="toggle">purchase</button>
   <h1 v-if="awesome">Add item to cart</h1>
@@ -47,7 +55,11 @@ export default {
     <p>Player: {{ todoId }}</p>
     <button @click="todoId++">Next Player</button>
     <p v-if="!todoData">Loading...</p>
-    <pre v-else>{{ todoData }}</pre>
+    <pre v-else
+      >{{ todoData.first_name }} {{ todoData.last_name }} ( {{
+        todoData.team.full_name
+      }} )</pre
+    >
   </div>
 </template>
 <style></style>
