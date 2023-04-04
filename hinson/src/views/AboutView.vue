a33c9981eec023f1a5694fd022244c028b0456a8
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2023-04-03 21:33:31 -0400

    a

--- hinson/src/views/AboutView.vue
+++ hinson/src/views/AboutView.vue
@@ -65,7 +65,10 @@ export default {
   </div>
   <div id="cart">
     <h1>Cart</h1>
-    <li></li>
+    <li>{{ todoData.first_name }} {{ todoData.last_name }} ( {{
+        todoData.team.full_name
+      }} ) 
+      Price : $150</li>
   </div>
 </template>
 <style>
